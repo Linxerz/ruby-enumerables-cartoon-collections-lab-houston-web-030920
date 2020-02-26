03ca7f73e637b0a83d5b3440e158a3ad39f4f3c3
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 01:17:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
+require 'pp'
+
 def roll_call_dwarves(array)
-  array.each_with_index{|dwarf,index| puts "#{index + 1}. #{dwarf}"}
+  pp array.each_with_index{|dwarf,index| puts "#{index + 1}. #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
