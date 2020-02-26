72e4aa0572fc0e0bc3e1018109babe4dda249749
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 01:26:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,12 +8,11 @@ def summon_captain_planet(array)
   array.collect{|word| word = word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|word| word.length > 4? }
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-  
