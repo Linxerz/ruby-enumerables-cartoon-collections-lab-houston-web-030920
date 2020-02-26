ea4a26623a06e1c6a677ded21a60082e8f64defd
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 01:32:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,10 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    array.any?{|item| item == cheese}
+  end
 end
